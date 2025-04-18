1750404077fe9862471b181e3fb55b6bc90c5b09
Juliusz Gonera <jgonera@gmail.com>
2025-04-17 22:13:42 -0700

    Add React rules to ESLint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,17 @@
 import eslintConfigJgonera from "eslint-config-jgonera"
+import reactPlugin from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
 
-export default [...eslintConfigJgonera]
+export default [
+  ...eslintConfigJgonera,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.tsx"],
+    plugins: { "react-hooks": reactHooks },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+]
