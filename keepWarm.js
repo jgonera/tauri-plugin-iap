c7159a2cc3b65ea0594b0be77be259a89d67da4c
Juliusz Gonera <jgonera@gmail.com>
2025-05-01 10:06:14 -0700

    Catch errors in keep warm script

--- keepWarm.js
+++ keepWarm.js
@@ -1,8 +1,12 @@
 while (true) {
   console.log(`${new Date().toISOString()} Pinging /api/version`)
-  await fetch(
-    "https://ollama-minicpm-v-31109354798.us-central1.run.app/api/version",
-  )
+  try {
+    await fetch(
+      "https://ollama-minicpm-v-31109354798.us-central1.run.app/api/version",
+    )
+  } catch (e) {
+    console.error(`${new Date().toISOString()} ${e}`)
+  }
   console.log(`${new Date().toISOString()} Done. Waiting...`)
   await new Promise((r) => setTimeout(r, 14 * 60 * 1000))
 }
