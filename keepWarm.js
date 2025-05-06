d805f81dff8e5f04185a192365b0446a0d83c505
Juliusz Gonera <jgonera@gmail.com>
2025-05-06 11:02:29 -0700

    Revert to /version in keepWarm.js
    The problem is the interval, not the endpoint.

--- keepWarm.js
+++ keepWarm.js
@@ -1,20 +1,8 @@
 while (true) {
-  console.log(`${new Date().toISOString()} Pinging /api/generate`)
+  console.log(`${new Date().toISOString()} Pinging /api/version`)
   try {
     await fetch(
-      "https://ollama-minicpm-v-31109354798.us-central1.run.app/api/generate",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          model: "minicpm-v:8b-2.6-q4_K_M",
-          prompt: "Respond with just one word: hi",
-          options: {
-            temperature: 0.01,
-            top_k: 100,
-            top_p: 0.8,
-          },
-        }),
-      },
+      "https://ollama-minicpm-v-31109354798.us-central1.run.app/api/version",
     )
   } catch (e) {
     console.error(`${new Date().toISOString()} ${e}`)
