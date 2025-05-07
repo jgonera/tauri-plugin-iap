6e79adebff1a0d91b70289073f14f9e6be6dda83
Juliusz Gonera <jgonera@gmail.com>
2025-05-06 23:57:15 -0700

    Run edge to edge, both on Android and iOS

--- src-tauri/gen/android/app/src/main/java/com/scribblescan/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/scribblescan/MainActivity.kt
@@ -1,3 +1,12 @@
 package com.scribblescan
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // https://github.com/tauri-apps/tauri/issues/12182#issuecomment-2584603995
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+    }
+}
