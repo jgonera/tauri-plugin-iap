6ed066a2ef5c9ac6e441265a94d023fc812ec8f6
Juliusz Gonera <jgonera@gmail.com>
2025-05-09 16:46:21 -0700

    Replace prompt and confirm
    Disable edge-to-edge on Android for now because of:
    https://github.com/tauri-apps/tauri/issues/7868#issuecomment-2868162077

--- src-tauri/gen/android/app/src/main/java/com/scribblescan/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/scribblescan/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.scribblescan
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+// import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // https://github.com/tauri-apps/tauri/issues/12182#issuecomment-2584603995
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         super.onCreate(savedInstanceState)
     }
 }
--- src/components/DocDrawer.tsx
+++ src/components/DocDrawer.tsx
@@ -1,4 +1,6 @@
 import { Bug, PencilSimpleLine, Trash } from "@phosphor-icons/react"
+import clsx from "clsx"
+import { useEffect, useRef, useState } from "react"
 import { Link, useNavigate } from "react-router"
 import { Drawer } from "vaul"
 
@@ -16,9 +18,18 @@ interface DocDrawerProps {
 export default function DocDrawer({ doc, isOpen, onDelete }: DocDrawerProps) {
   const navigate = useNavigate()
   const { deleteDoc, renameDoc } = useStore()
+  const [state, setState] = useState<"default" | "delete" | "rename">("default")
+  const nameInputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (isOpen) {
+      setState("default")
+    }
+  }, [isOpen])
 
   return (
     <Drawer.Root
+      repositionInputs={false}
       open={isOpen}
       onOpenChange={(isOpen) => {
         if (!isOpen) {
@@ -34,45 +45,96 @@ export default function DocDrawer({ doc, isOpen, onDelete }: DocDrawerProps) {
             <Drawer.Description className={classes.description}>
               Actions for {doc.name}
             </Drawer.Description>
-            <ul>
-              <li>
-                <button
-                  onClick={() => {
-                    const name = prompt("New name:", doc.name)
-
-                    if (name !== null && name !== "") {
-                      void renameDoc(doc.id, name)
-                      void navigate(-1)
-                    }
-                  }}
-                >
-                  <PencilSimpleLine size={24} />
-                  Rename
-                </button>
-              </li>
-              <li>
-                <button
-                  className={classes.delete}
-                  onClick={() => {
-                    if (
-                      confirm(`Are you sure you want to delete "${doc.name}"?`)
-                    ) {
+            {state === "default" && (
+              <ul>
+                <li>
+                  <button
+                    onClick={() => {
+                      setState("rename")
+                    }}
+                  >
+                    <PencilSimpleLine size={24} />
+                    Rename
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className={classes.deleteMenu}
+                    onClick={() => {
+                      setState("delete")
+                    }}
+                  >
+                    <Trash size={24} /> Delete
+                  </button>
+                </li>
+                {import.meta.env.DEV && (
+                  <li>
+                    <Link to="/debug">
+                      <Bug size={24} /> Debug
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            )}
+            {state === "delete" && (
+              <>
+                <p>
+                  Are you sure you want to delete <strong>{doc.name}</strong>?
+                </p>
+                <div className={classes.buttonBar}>
+                  <button
+                    className={clsx(classes.button, classes.text)}
+                    onClick={() => {
+                      setState("default")
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className={clsx(classes.button, classes.danger)}
+                    onClick={() => {
                       void deleteDoc(doc.id)
                       onDelete()
-                    }
-                  }}
-                >
-                  <Trash size={24} /> Delete
-                </button>
-              </li>
-              {import.meta.env.DEV && (
-                <li>
-                  <Link to="/debug">
-                    <Bug size={24} /> Debug
-                  </Link>
-                </li>
-              )}
-            </ul>
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </>
+            )}
+            {state === "rename" && (
+              <>
+                <input
+                  className={classes.input}
+                  ref={nameInputRef}
+                  type="text"
+                  defaultValue={doc.name}
+                />
+                <div className={classes.buttonBar}>
+                  <button
+                    className={clsx(classes.button, classes.text)}
+                    onClick={() => {
+                      setState("default")
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className={clsx(classes.button, classes.accent)}
+                    onClick={() => {
+                      const name = nameInputRef.current?.value
+
+                      if (name !== undefined && name !== "") {
+                        void renameDoc(doc.id, name)
+                        void navigate(-1)
+                      }
+                    }}
+                  >
+                    Rename
+                  </button>
+                </div>
+              </>
+            )}
           </nav>
         </Drawer.Content>
       </Drawer.Portal>
