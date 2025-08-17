9a6284a3e5b13f56d86149ac90551951d63abbc7
Juliusz Gonera <jgonera@gmail.com>
2025-08-16 17:57:58 -0700

    Remove react-aria-components
    Not really using them now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react"
 import { usePreventScroll } from "react-aria"
-import { RouterProvider } from "react-aria-components"
-import { Route, Routes, useHref, useNavigate } from "react-router"
+import { Route, Routes } from "react-router"
 
 import Camera from "@/Camera"
 import Debug from "@/Debug"
@@ -17,34 +16,30 @@ export default function App() {
     void warmUpOCR()
   }, [])
 
-  const navigate = useNavigate()
   usePreventScroll()
 
   return (
     <React.StrictMode>
-      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-      <RouterProvider navigate={navigate} useHref={useHref}>
-        <Routes>
-          <Route path="/" element={<List />} />
-          <Route path="/camera" element={<Camera />} />
-          <Route path="/debug" element={<Debug />} />
-          <Route path="/doc/:id" element={<Doc />} />
-          <Route path="/doc/:id/camera" element={<Camera />} />
-          <Route path="/doc/:id/doc-drawer" element={<Doc showDocDrawer />} />
-          <Route path="/doc/:id/page/:pageId" element={<Page />} />
-          <Route
-            path="/doc/:id/page/:pageId/delete-drawer"
-            element={<Page showDeleteDrawer />}
-          />
-          <Route path="/list/:id/doc-drawer" element={<List showDocDrawer />} />
-          <Route path="/search" element={<Search />} />
-          <Route
-            path="/search/:id/doc-drawer"
-            element={<Search showDocDrawer />}
-          />
-          <Route path="/subscribe" element={<Subscribe />} />
-        </Routes>
-      </RouterProvider>
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="/camera" element={<Camera />} />
+        <Route path="/debug" element={<Debug />} />
+        <Route path="/doc/:id" element={<Doc />} />
+        <Route path="/doc/:id/camera" element={<Camera />} />
+        <Route path="/doc/:id/doc-drawer" element={<Doc showDocDrawer />} />
+        <Route path="/doc/:id/page/:pageId" element={<Page />} />
+        <Route
+          path="/doc/:id/page/:pageId/delete-drawer"
+          element={<Page showDeleteDrawer />}
+        />
+        <Route path="/list/:id/doc-drawer" element={<List showDocDrawer />} />
+        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search/:id/doc-drawer"
+          element={<Search showDocDrawer />}
+        />
+        <Route path="/subscribe" element={<Subscribe />} />
+      </Routes>
     </React.StrictMode>
   )
 }
